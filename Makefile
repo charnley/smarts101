ad9ca76f1083c6f694392a505acdd5d2002847ae
Jimmy Kromann <jimmy@charnley.dk>
2026-03-10 20:57:16 +0100

    Init shadcn svelte5

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+node_modules:
+	pnpm i
+
+format:
+	npx prettier
+
+dev:
+	pnpm run dev --open
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+		<header class="flex h-16 shrink-0 items-center gap-2">
+			<div class="flex items-center gap-2 px-4">
+				<Sidebar.Trigger class="-ms-1" />
+				<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+				<!-- <Breadcrumb.Root> -->
+				<!-- 	<Breadcrumb.List> -->
+				<!-- 		<Breadcrumb.Item class="hidden md:block"> -->
+				<!-- 			<Breadcrumb.Link href="##">Building Your Application</Breadcrumb.Link> -->
+				<!-- 		</Breadcrumb.Item> -->
+				<!-- 		<Breadcrumb.Separator class="hidden md:block" /> -->
+				<!-- 		<Breadcrumb.Item> -->
+				<!-- 			<Breadcrumb.Page>Data Fetching</Breadcrumb.Page> -->
+				<!-- 		</Breadcrumb.Item> -->
+				<!-- 	</Breadcrumb.List> -->
+				<!-- </Breadcrumb.Root> -->
+			</div>
+		</header>
+<div class="gap-4 p-4 pt-0">
+{@render children()}
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
+
