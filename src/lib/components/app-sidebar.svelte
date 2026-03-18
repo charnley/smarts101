f309cd27d7bc485f7227ce50f6898cd1f8ed88b5
Jimmy Kromann <jimmy@charnley.dk>
2026-03-18 13:29:55 +0100

    Removed unused imports and commented code from app-sidebar.

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -2,14 +2,8 @@
 	// @ts-nocheck
 	import BookOpenIcon from '@lucide/svelte/icons/book-open';
 	import BookOpenCheckIcon from '@lucide/svelte/icons/book-open-check';
-	import BotIcon from '@lucide/svelte/icons/bot';
 	import InfoIcon from '@lucide/svelte/icons/info';
-	import ChartPieIcon from '@lucide/svelte/icons/chart-pie';
 	import FlaskConicalIcon from '@lucide/svelte/icons/flask-conical';
-	import FrameIcon from '@lucide/svelte/icons/frame';
-	import MapIcon from '@lucide/svelte/icons/map';
-	import Settings2Icon from '@lucide/svelte/icons/settings-2';
-	import SquareTerminalIcon from '@lucide/svelte/icons/square-terminal';
 
 	const data = {
 		navMain: [
@@ -72,35 +66,12 @@
 <script>
 	// @ts-nocheck
 	import NavMain from './nav-main.svelte';
-	import NavProjects from './nav-projects.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	let { ref = $bindable(null), ...restProps } = $props();
 </script>
 
 <Sidebar.Root bind:ref variant="inset" {...restProps}>
-	<!-- <Sidebar.Header> -->
-	<!-- 	<Sidebar.Menu> -->
-	<!-- 		<Sidebar.MenuItem> -->
-	<!-- 			<Sidebar.MenuButton size="lg"> -->
-	<!-- 				{#snippet child({ props })} -->
-	<!-- 					<a href="##" {...props}> -->
-	<!-- 						<div -->
-	<!-- 							class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground" -->
-	<!-- 						> -->
-	<!-- 							<CommandIcon class="size-4" /> -->
-	<!-- 						</div> -->
-	<!-- 						<div class="grid flex-1 text-start text-sm leading-tight"> -->
-	<!-- 							<span class="truncate font-medium">Acme Inc</span> -->
-	<!-- 							<span class="truncate text-xs">Enterprise</span> -->
-	<!-- 						</div> -->
-	<!-- 					</a> -->
-	<!-- 				{/snippet} -->
-	<!-- 			</Sidebar.MenuButton> -->
-	<!-- 		</Sidebar.MenuItem> -->
-	<!-- 	</Sidebar.Menu> -->
-	<!-- </Sidebar.Header> -->
 	<Sidebar.Content>
 		<NavMain items={data.navMain} />
-		<!-- <NavProjects projects={data.projects} /> -->
 	</Sidebar.Content>
 </Sidebar.Root>
